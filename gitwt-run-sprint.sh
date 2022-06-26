79129b42b2c2750213d32b334a6572d7a39de57e
Marcio Santos <s.marcio.j@gmail.com>
2022-06-26 20:52:32 -0300

    [gitwt] Adding more features and developer working on sprints

--- gitwt-run-sprint.sh
+++ gitwt-run-sprint.sh
@@ -32,3 +32,27 @@ COMMITI=3
 COMMITF=4
 #echo "---> [GitWT] Developer $USER is finishing feature $FEATURE..."
 bash $BASEDIR/gitwt-finish-feature.sh $PROJECT_NAME $USER $FEATURE $COMMITI $COMMITF
+
+#--------------
+USER=developer1
+FEATURE=$SPRINT-f3
+COMMITI=1
+COMMITF=2
+#echo "---> [GitWT] Developer $USER is finishing feature $FEATURE..."
+bash $BASEDIR/gitwt-finish-feature.sh $PROJECT_NAME $USER $FEATURE $COMMITI $COMMITF
+
+#--------------
+USER=developer2
+FEATURE=$SPRINT-f2
+COMMITI=4
+COMMITF=5
+#echo "---> [GitWT] Developer $USER is working on feature $FEATURE..."
+bash $BASEDIR/gitwt-finish-feature.sh $PROJECT_NAME $USER $FEATURE $COMMITI $COMMITF
+
+#--------------
+USER=developer2
+FEATURE=$SPRINT-f4
+COMMITI=1
+COMMITF=1
+#echo "---> [GitWT] Developer $USER is working on feature $FEATURE..."
+bash $BASEDIR/gitwt-finish-feature.sh $PROJECT_NAME $USER $FEATURE $COMMITI $COMMITF
